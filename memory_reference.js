0d94ca7e2eb426571b4426e02305f0ab0857f40c
beluyencode <longlc956@gmail.com>
2024-02-15 00:18:22 +0700

    1

--- memory_reference.js
+++ memory_reference.js
@@ -6,3 +6,11 @@ var b = a;
 b.name = 'b';
 
 console.log(a.name); // Output: "b"
+
+var arr = [{ name: 'a' }, { name: 'b' }];
+
+var arr2 = [...arr];
+arr2[0].name = 'c';
+
+
+console.log(arr[0].name); // Output: "c"
